85a45a042c599a3147f179d6622dbbb764e91522
GarytheNoob on ArchLinux <garythenoob@outlook.com>
2023-12-30 15:32:17 +0800

    fix: qv2ray launch

--- script/script/serviceInit.sh
+++ script/script/serviceInit.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 pgrep -x picom > /dev/null || picom --experimental-backends &
-killall v2ray qv2ray
-qv2ray &
+
+if [ -z "$(pgrep -x qv2ray)" ]; then
+    qv2ray &
+fi
 pgrep -x blueberry-tray >> /dev/null || blueberry-tray 
 # clash &
