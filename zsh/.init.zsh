18657f28299699181f7966cdb88d500b38deac7c
GarytheNoob on ArchLinux <garythenoob@outlook.com>
2024-01-12 21:51:22 +0800

    add: feat checking tmux sessions

--- zsh/.init.zsh
+++ zsh/.init.zsh
@@ -19,11 +19,15 @@ export termapp=$(basename $(ps -p $(ps -p $$ -o ppid=) o args= | awk '{print $1}
 
 if [[ "$termapp" != "tmux" ]]; then
     echo -e "\n  \033[01;34mWelcome to $termapp, $USER! \033[0m\n"
+    if [ -n "$(tmux list-sessions 2>/dev/null)" ]; then
+        echo "Running tmux sessions:"
+        echo "   $(tmux ls)"
+        alias tmuxa='tmux attach-session -t'
+        echo "Attach to sessions quickly with \033[04;33mtmuxa\033[0m."
+    fi
+    # change ls colors
 fi
 
 
-# change ls colors
 LS_COLORS=$LS_COLORS:'di=0;36:ow=1;4;36' ; export LS_COLORS
 
-# variable
-export EDITOR=nvim
