6f7c14a4fbc4d5ce3eea269f25c35ccd1b09fbd8
GarytheNoob on ArchLinux <garythenoob@outlook.com>
2024-01-28 16:22:53 +0800

    del: qv2ray in rofi menu

--- script/script/configservice.sh
+++ script/script/configservice.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 manageService(){
-    menu="qv2ray\ndunst\nfcitx5\npicom" # REMOVED sxhkd to avoid danger
+    menu="dunst\nfcitx5\npicom" # REMOVED sxhkd to avoid danger
     echo -e "$menu" | rofi -dmenu -p "Manage services"
 }
 
