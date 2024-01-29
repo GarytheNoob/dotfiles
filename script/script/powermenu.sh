d0ed3722a1e514d9b8c0fff928a7b281b2a05013
GarytheNoob on ArchLinux <garythenoob@outlook.com>
2024-01-29 17:43:53 +0800

    del: blur for lockscreen

--- script/script/powermenu.sh
+++ script/script/powermenu.sh
@@ -96,7 +96,7 @@ case ${chosen} in
         ;;
     $lock)
 		if [[ -x '/usr/bin/betterlockscreen' ]]; then
-			betterlockscreen -l blur
+			betterlockscreen -l
 		elif [[ -x '/usr/bin/i3lock' ]]; then
 			i3lock
 		fi
