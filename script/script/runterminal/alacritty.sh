2fea455b13b8eca09c8d71901d45b7e4987913c3
GarytheNoob on ArchLinux <garythenoob@outlook.com>
2024-02-03 23:02:22 +0800

    add: floating window?

--- script/script/runterminal/alacritty.sh
+++ script/script/runterminal/alacritty.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
+# TODO: Switch
+
 if [[ "$THEME" == "onedark" ]]; then
-    exec alacritty --config-file ~/.config/alacritty/onedark.yml
+    config_path="~/.config/alacritty/onedark.yml"
 elif [[ "$THEME" == "lovelive" ]]; then
-    exec alacritty --config-file ~/.config/alacritty/lovelive.yml
+    config_path="~/.config/alacritty/lovelive.yml"
 else
-    exec alacritty
+    config_path="~/.config/alacritty/alacritty.yml"
+fi
+
+if [[ "$#" -gt 0 ]]; then
+    # TODO: Switch
+    if [[ "$1" == "--qalc" ]]; then
+        exec alacritty --config-file $config_path --class floating --command qalc
+    fi
+else
+    if [[ "$THEME" == "onedark" ]]; then
+        exec alacritty --config-file $config_path
+    elif [[ "$THEME" == "lovelive" ]]; then
+        exec alacritty --config-file $config_path
+    else
+        exec alacritty
+    fi
 fi
