8ec1dfe3ac57ab66a71608e1c12c3db877a432b0
GarytheNoob on ArchLinux <garythenoob@outlook.com>
2024-02-08 11:34:26 +0800

    change: add picom rule for obsidian

--- zsh/.fzf.zsh
+++ zsh/.fzf.zsh
@@ -15,7 +15,5 @@
 # GitHub: https://github.com/GarytheNoob
 
 export FZF_DEFAULT_OPTS='--preview "[[ $(file --mime {}) =~ binary ]] && echo {} is a binary file || (bat --style=numbers --color=always {} || highlight -O ansi -l {} || cat {}) 2> /dev/null | head -500"'
-export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -l -g ""'
-export FZF_COMPLETION_TRIGGER='~~'
-
+export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
 
