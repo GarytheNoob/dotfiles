ebce32aa22a9b52627997f043a11560e71136124
GarytheNoob on ArchLinux <garythenoob@outlook.com>
2024-03-16 10:36:47 +0800

    wip: switch alert

---
+++ script/script/alert/switchmute.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+cache_path=~/.cache/alert_status
+
+if [ ! -f "$cache_path" ]; then 
+    echo "unmuted" > $cache_path
+fi
+
+status=$(/bin/cat $cache_path)
+
+case "$status" in
+    "muted")
+        ;;
+    "unmuted")
+        echo 
+        ;;
+esac
+
+if [[ $# -gt 0 && $1 == "-q" ]]; then
+    case "$status" in
+        "muted")
+            echo " "
+            ;;
+        "unmuted")
+            echo " "
+            ;;
+    esac
+else
+    case "$status" in
+        "muted")
+            echo "unmuted" > $cache_path
+            ;;
+        "unmuted")
+            echo "muted" > $cache_path
+            ;;
+    esac
+fi
+
+
