3be2a865d62e1308c9971249971acc6226021a3e
ZhangXuecheng <zxc0gary@connect.hku.hk>
2026-01-23 12:42:01 +0800

    change: wezterm font scale; colorscheme

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,23 +1,13 @@
 local wezterm = require("wezterm")
 
-local get_theme = function ()
-    local env_theme = os.getenv("THEME")
-    if env_theme then
-        if env_theme == "onedark" then
-            return 'OneDark (base16)'
-        end
-    end
-    return 'Default Dark (base16)'
-end
-
 local config = {
-    font_size = 14,
+    font_size = 18,
     font = wezterm.font_with_fallback({
         { family = 'JetBrainsMono Nerd Font', weight = 'Regular' },
         { family = 'Noto Sans CJK SC',        weight = 'Bold'    },
         'Noto Color Emoji',
     }),
-    color_scheme = get_theme(),
+    color_scheme = 'OneDark (base16)',
 
     window_padding = {
         left = 0,
@@ -29,6 +19,7 @@ local config = {
     enable_tab_bar = false,
     hide_tab_bar_if_only_one_tab = true,
     window_decorations = "RESIZE",
+    window_close_confirmation = 'NeverPrompt',
 }
 
 return config
